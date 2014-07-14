c0ccb8a8bf922c7becaa4e0a84ab02825d510b82
steve_siebert <steve_siebert@GORD14NWKIGD097>
2014-07-14 08:44:47 -0400

    added test for PDF thumbnail generation

--- src/test/java/org/geoint/thumbnailer/ThumbnailerTest.java
+++ src/test/java/org/geoint/thumbnailer/ThumbnailerTest.java
@@ -14,47 +14,63 @@ public class ThumbnailerTest {
     @Test
     public void testPNG() throws Exception {
         File outFile = new File(System.getProperty("java.io.tmpdir")
-                + File.separator + "outImg.jpg");
+                + File.separator + "outPNG.jpg");
         try (InputStream in = ThumbnailerTest.class.getResourceAsStream("/example_img.png")) {
             Thumbnailer.thumb(in).height(400).create(outFile);
             assertTrue(outFile.exists());
-            Desktop.getDesktop().open(outFile);
+//            Desktop.getDesktop().open(outFile);
         } finally {
-//            if (outFile.exists()) {
-//                outFile.delete();
-//            }
+            if (outFile.exists()) {
+                outFile.delete();
+            }
         }
     }
 
     @Test
     public void testPPT() throws Exception {
         File outFile = new File(System.getProperty("java.io.tmpdir")
-                + File.separator + "outImg.jpg");
+                + File.separator + "outPPT.jpg");
         String testFileName = "example_ppt.ppt";
         try (InputStream in = ThumbnailerTest.class.getResourceAsStream("/" + testFileName)) {
             Thumbnailer.thumb(in).height(400).create(outFile);
             assertTrue(outFile.exists());
-            Desktop.getDesktop().open(outFile);
+//            Desktop.getDesktop().open(outFile);
         } finally {
-//            if (outFile.exists()) {
-//                outFile.delete();
-//            }
+            if (outFile.exists()) {
+                outFile.delete();
+            }
         }
     }
 
     @Test
     public void testPPTX() throws Exception {
         File outFile = new File(System.getProperty("java.io.tmpdir")
-                + File.separator + "outImg.jpg");
+                + File.separator + "outPPTX.jpg");
         String testFileName = "example_pptx.pptx";
         try (InputStream in = ThumbnailerTest.class.getResourceAsStream("/" + testFileName)) {
             Thumbnailer.thumb(in).height(400).create(outFile);
             assertTrue(outFile.exists());
-            Desktop.getDesktop().open(outFile);
+//            Desktop.getDesktop().open(outFile);
         } finally {
-//            if (outFile.exists()) {
-//                outFile.delete();
-//            }
+            if (outFile.exists()) {
+                outFile.delete();
+            }
+        }
+    }
+    
+    @Test
+    public void testPDF() throws Exception {
+        File outFile = new File(System.getProperty("java.io.tmpdir")
+                + File.separator + "outPDF.jpg");
+        String testFileName = "example_pdf.pdf";
+        try (InputStream in = ThumbnailerTest.class.getResourceAsStream("/" + testFileName)) {
+            Thumbnailer.thumb(in).height(400).create(outFile);
+            assertTrue(outFile.exists());
+//            Desktop.getDesktop().open(outFile);
+        } finally {
+            if (outFile.exists()) {
+                outFile.delete();
+            }
         }
     }
 }
